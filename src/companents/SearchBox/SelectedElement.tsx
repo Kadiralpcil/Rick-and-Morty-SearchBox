5fb11e818feb2ba70591dc37206e2c16f64fb363
Kadir Alp Çil <kadiralpcil@gmail.com>
2024-03-29 13:04:36 +0300

    added responsive design for selectedItems

--- src/companents/SearchBox/SelectedElement.tsx
+++ src/companents/SearchBox/SelectedElement.tsx
@@ -10,6 +10,7 @@ import { FaAngleDown } from "react-icons/fa";
 
 //Style
 import styles from "./index.module.css";
+import useWindowDimensions from "../../Hooks/useWindowDimensions";
 
 interface selectedElementProps {
     selectedCharacters: Character[];
@@ -23,9 +24,32 @@ const SelectedElement = forwardRef<HTMLDivElement, selectedElementProps>(
         { selectedCharacters, handleAddRemoveCharacter, setDropdown, dropdown },
         ref,
     ) => {
+
+        const { width } = useWindowDimensions();
+        console.log(width)
+
+        const getSlicedChracters = (where: "SearchBox" | "Dropdown") => {
+            switch (where) {
+                case "SearchBox":
+                    if (width > 640) {
+                        return selectedCharacters.slice(0, 2);
+                    } else {
+                        return selectedCharacters.slice(0, 1);
+                    }
+                case "Dropdown":
+                    if (width > 640) {
+                        return selectedCharacters.slice(2);
+                    } else {
+                        return selectedCharacters.slice(1);
+                    }
+                default:
+                    return selectedCharacters
+            }
+        }
+
         return (
             <>
-                {selectedCharacters.slice(0, 2).map((char, index) => (
+                {getSlicedChracters("SearchBox").map((char, index) => (
                     <div className={styles.selected_element} key={index}>
                         <div className={styles.char_name}>{char.name}</div>
                         <span
@@ -36,19 +60,19 @@ const SelectedElement = forwardRef<HTMLDivElement, selectedElementProps>(
                         </span>
                     </div>
                 ))}
-                {selectedCharacters.length > 2 && (
+                {selectedCharacters.length > (width < 640 ? 1 : 2) && (
                     <div className={styles.selected_wrapper}>
                         <div className={styles.selected_element}>
                             <div onClick={() => setDropdown()} className={styles.char_name}>
-                                <span>+{selectedCharacters.length - 2}</span>
+                                <span>+{selectedCharacters.length - (width < 640 ? 1 : 2)}</span>
                                 <span className={styles.dropdown_down_icon}>
                                     <FaAngleDown />
                                 </span>
                             </div>
                         </div>
                         {dropdown && (
-                            <div ref={ref} className={styles.dropdown_list} tabIndex={0}>
-                                {selectedCharacters.slice(2).map((char) => (
+                            <div ref={ref} className={styles.dropdown_list} >
+                                {getSlicedChracters("Dropdown").map((char) => (
                                     <div
                                         className={styles.selected_element_dropdown}
                                         key={char.id}
